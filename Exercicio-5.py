a0df82683b64485bd5811b9f8efba24911060073
Lucas Lourenço <lucas.g.j.l@hotmail.com>
2024-03-11 16:07:20 -0300

    Envio das resposta do Estagio de Desenvolvimento

--- Exercicio-5.py
+++ Exercicio-5.py
@@ -1,6 +1,5 @@
 #5) Escreva um programa que inverta os caracteres de um string.
 
-
 #IMPORTANTE:
 
 #a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
